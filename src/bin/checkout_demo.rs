d5639ca6075e19d6dca6e1df4c78e509a2603b6f
Charlie Wilkin <charlie@busyboredom.com>
2021-08-20 21:05:00 -0400

    Fix payment ID retrieval

--- src/bin/checkout_demo.rs
+++ src/bin/checkout_demo.rs
@@ -1,34 +1,21 @@
-use std::convert::TryInto;
-use std::str::FromStr;
+use std::collections::HashMap;
 
-use monero::blockdata::transaction;
-use monero::consensus::encode;
-use monero::cryptonote::hash::keccak_256;
 use monero::util::address::PaymentId;
-use monero::util::{address, key};
 use qrcode::render::svg;
 use qrcode::QrCode;
-use serde_json::{json, Value};
 use tokio::fs;
 use tokio::time;
 
-use xmr_checkout::{BlockScanner, BlockScannerBuilder};
+use xmr_checkout::{BlockScanner, BlockScannerBuilder, Payment};
 
 #[tokio::main]
 async fn main() {
     // Prepare Viewkey.
     let mut viewkey_string = include_str!("../../../secrets/xmr_private_viewkey.txt").to_string();
     viewkey_string.pop();
-    let private_viewkey = key::PrivateKey::from_str(&viewkey_string).unwrap();
 
-    // Prepare Spendkey.
-    let public_spendkey = key::PublicKey::from_str(
-        "dd4c491d53ad6b46cda01ed6cb9bac57615d9eac8d5e4dd1c0363ac8dfd420a7",
-    )
-    .unwrap();
-
-    let block_scanner = BlockScannerBuilder::new()
-        .daemon_url("https://busyboredom.com:18081")
+    let mut block_scanner = BlockScannerBuilder::new()
+        .daemon_url("http://busyboredom.com:18081")
         .private_viewkey(&viewkey_string)
         .public_spendkey("dd4c491d53ad6b46cda01ed6cb9bac57615d9eac8d5e4dd1c0363ac8dfd420a7")
         .scan_rate(1000)
@@ -36,7 +23,7 @@ async fn main() {
 
     // Get a new integrated address, and the payment ID contained in it.
     let (address, payment_id) = block_scanner.new_integrated_address();
-    println!("Payment ID: {:#?}", payment_id);
+    println!("Payment ID: {}", payment_id);
 
     // Render a QR code for the new address.
     let qr = QrCode::new(address).unwrap();
@@ -49,18 +36,13 @@ async fn main() {
 
     // Below this is old test code --------------------------------------------------------
 
-    // Combine into keypair.
-    let viewpair = key::ViewPair {
-        view: private_viewkey,
-        spend: public_spendkey,
-    };
-
     let mut blockscan_interval = time::interval(time::Duration::from_secs(1));
     loop {
         blockscan_interval.tick().await;
         let current_height = get_current_height().await.unwrap();
-        println!("{:?}", current_height);
-        let amount = scan_block_transactions(current_height, &viewpair)
+        let current_height = 2431442;
+        println!("Current Block: {}", current_height);
+        let amount = scan_block_transactions(current_height, &mut block_scanner)
             .await
             .unwrap();
         if amount != 0 {
@@ -87,86 +69,27 @@ async fn get_current_height() -> Result<u64, reqwest::Error> {
 
 async fn scan_block_transactions(
     height: u64,
-    viewpair: &key::ViewPair,
+    block_scanner: &mut BlockScanner,
 ) -> Result<u64, reqwest::Error> {
-    let client = reqwest::Client::new();
+    let block = block_scanner.get_block(height).await?;
+    let transactions = block_scanner.get_block_transactions(block).await?;
+    println!("Transactions: {}", transactions.len());
 
-    let request_body = r#"{"jsonrpc":"2.0","id":"0","method":"get_block","params":{"height":"#
-        .to_owned()
-        + &height.to_string()
-        + "}}";
-    let res = client
-        .post("http://busyboredom.com:18081/json_rpc")
-        .body(request_body)
-        .send()
-        .await?;
+    // TODO: write a function for blockscanner that lets me insert this payment.
+    let payment_id = PaymentId::from_slice(&hex::decode("33d2a0f45130c85b").unwrap());
+    let payment = Payment {
+        payment_id: payment_id,
+        expected_amount: 1,
+        paid_amount: 0,
+        confirmations_required: 1,
+        confirmations_recieved: 0,
+        expiration_block: 10,
+    };
+    block_scanner.scan_transactions(transactions).await;
 
-    let res = res.json::<serde_json::Value>().await?;
+    let amount = payment.paid_amount;
 
-    // Get block transactions in sets of 100 or less (the restriced RPC maximum).
-    let mut transaction_hexes = vec![];
-    let mut transaction_hashes = vec![];
-    if let Some(tx_hashes) = res["result"]["tx_hashes"].as_array() {
-        transaction_hashes = tx_hashes.to_vec();
-    }
-    for i in 0..(transaction_hashes.len() / 100 + 1) {
-        let starting_index: usize = i * 100;
-        let ending_index: usize = std::cmp::min(100 * (i + 1), transaction_hashes.len());
-        let request_body = r#"{"txs_hashes":"#.to_owned()
-            + &json!(transaction_hashes[starting_index..ending_index]).to_string()
-            + "}";
-        let res = client
-            .post("http://busyboredom.com:18081/get_transactions")
-            .body(request_body)
-            .send()
-            .await?;
+    println!("Amount: {}", monero::Amount::from_pico(amount).as_xmr());
 
-        let res = res.json::<serde_json::Value>().await?;
-
-        // Add these transactions to the total list.
-        if let Some(hexes) = res["txs_as_hex"].as_array() {
-            transaction_hexes.append(&mut hexes.to_vec());
-        }
-    }
-
-    let mut total_amount: u64 = 0;
-    for tx_hex_json in transaction_hexes {
-        let tx_hex_str = tx_hex_json.as_str().unwrap();
-        let tx_hex = hex::decode(tx_hex_str).unwrap();
-        let tx: transaction::Transaction = encode::deserialize(&tx_hex).unwrap();
-        let owned_outputs = tx.check_outputs(&viewpair, 0..2, 0..3).unwrap();
-        for output in &owned_outputs {
-            total_amount += output
-                .amount()
-                .expect("Failed to read amount from owned output");
-        }
-
-        // Generate and display the SubFields (the parsed "extra" section) if applicable.
-        let payment_id: Option<address::PaymentId> = None;
-        if owned_outputs.len() == 1 {
-            let tx_extra = &tx.prefix().extra;
-            let transaction::ExtraField(subfields) = tx_extra;
-            for subfield in subfields {
-                println!("SubField: {:#?}", subfield);
-                if let transaction::SubField::Nonce(bytes) = subfield {
-                    // THIS STUFF DOESN'T WORK YET
-                    let shared_secret = tx.tx_pubkey().unwrap() * &(viewpair.view * 8u8);
-                    let mut key_bytes = shared_secret.as_bytes().to_vec();
-                    key_bytes.append(&mut hex::decode("8d").unwrap());
-                    let key = keccak_256(&key_bytes);
-                    let mut id_bytes = bytes.clone()[0..8].to_vec();
-                    id_bytes
-                        .iter_mut()
-                        .zip(key.iter())
-                        .for_each(|(x1, x2)| *x1 ^= *x2);
-                    let payment_id = Some(address::PaymentId::from_slice(&id_bytes));
-                    println!("Payment ID: {:#?}", &id_bytes)
-                }
-            }
-        }
-    }
-
-    println!("{}", monero::Amount::from_pico(total_amount).as_xmr());
-
-    Ok(total_amount)
+    Ok(amount)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
+use std::collections::HashMap;
 use std::str::FromStr;
 
-use monero::network::Network;
-use monero::util::{address, key};
+use monero::blockdata::transaction::{ExtraField, SubField};
+use monero::consensus::encode::deserialize;
+use monero::cryptonote::hash::keccak_256;
+use monero::util::address::PaymentId;
+use monero::Network::Mainnet;
+use reqwest;
+use serde_json;
 
 pub struct BlockScanner {
     daemon_url: String,
-    viewpair: key::ViewPair,
+    viewpair: monero::ViewPair,
+    payments: HashMap<PaymentId, Payment>,
     scan_rate: u64,
 }
 
@@ -14,29 +21,165 @@ impl BlockScanner {
         BlockScannerBuilder::default()
     }
 
-    pub fn new_integrated_address(&self) -> (String, [u8; 8]) {
-        let standard_address = address::Address::from_viewpair(Network::Mainnet, &self.viewpair);
+    pub fn new_integrated_address(&self) -> (String, String) {
+        let standard_address = monero::Address::from_viewpair(Mainnet, &self.viewpair);
 
-        let integrated_address = address::Address::integrated(
-            Network::Mainnet,
+        let integrated_address = monero::Address::integrated(
+            Mainnet,
             standard_address.public_spend,
             standard_address.public_view,
-            address::PaymentId::random(),
+            PaymentId::random(),
         );
         let payment_id = match integrated_address.addr_type {
-            address::AddressType::Integrated(id) => id,
+            monero::AddressType::Integrated(id) => id,
             _ => panic!("Integrated address malformed (no payment ID)"),
         };
 
-        (format!("{}", integrated_address), payment_id.to_fixed_bytes())
+        (format!("{}", integrated_address), hex::encode(&payment_id))
+    }
+
+    pub async fn get_block(&self, height: u64) -> Result<monero::Block, reqwest::Error> {
+        let client = reqwest::Client::new();
+
+        let request_body = r#"{"jsonrpc":"2.0","id":"0","method":"get_block","params":{"height":"#
+            .to_owned()
+            + &height.to_string()
+            + "}}";
+        let res = client
+            .post(self.daemon_url.to_owned() + "/json_rpc")
+            .body(request_body)
+            .send()
+            .await?;
+
+        let block_json = res.json::<serde_json::Value>().await?;
+        println!(
+            "Block request status: {}",
+            block_json["result"]["status"].as_str().unwrap()
+        );
+
+        let block_blob = block_json["result"]["blob"]
+            .as_str()
+            .expect("Failed to read block blob from json_rpc");
+
+        let block_bytes =
+            hex::decode(block_blob).expect("Failed to decode block blob from hex to bytes");
+
+        let block = deserialize(&block_bytes).expect("Failed to deserialize block blob");
+
+        Ok(block)
+    }
+
+    pub async fn get_block_transactions(
+        &self,
+        block: monero::Block,
+    ) -> Result<Vec<monero::Transaction>, reqwest::Error> {
+        // Get block transactions in sets of 100 or less (the restriced RPC maximum).
+        let client = reqwest::Client::new();
+        let mut transactions = vec![];
+        let transaction_hashes = block.tx_hashes;
+        println!("Transactions to fetch: {}", transaction_hashes.len());
+        for i in 0..(transaction_hashes.len() / 100 + 1) {
+            // Start and end indexes of the hashes we're grabbing for now.
+            let starting_index: usize = i * 100;
+            let ending_index: usize = std::cmp::min(100 * (i + 1), transaction_hashes.len());
+            println!("Transactions requested: {}", ending_index - starting_index);
+
+            // Build a json containing the hashes of the transactions we want.
+            let request_body = r#"{"txs_hashes":"#.to_owned()
+                + &serde_json::json!(transaction_hashes[starting_index..ending_index]
+                    .iter()
+                    .map(|x| hex::encode(x.as_bytes())) // Convert from monero::Hash to hex.
+                    .collect::<Vec<String>>())
+                .to_string()
+                + "}";
+            let res = client
+                .post("http://busyboredom.com:18081/get_transactions")
+                .body(request_body)
+                .send()
+                .await?;
+
+            let res = res.json::<serde_json::Value>().await?;
+            println!(
+                "Transaction request status: {}",
+                res["status"].as_str().unwrap()
+            );
+
+            // Add these transactions to the total list.
+            if let Some(hexes) = res["txs_as_hex"].as_array() {
+                for tx_json in hexes {
+                    let tx_str = tx_json
+                        .as_str()
+                        .expect("Failed to read transaction hex from json");
+                    let tx_hex = hex::decode(tx_str)
+                        .expect("Failed to decode transaction fron hex to bytes");
+                    let tx = deserialize(&tx_hex).expect("Failed to deserialize transaction");
+                    transactions.push(tx);
+                }
+            }
+        }
+
+        println!("Transactions fetched: {}", transactions.len());
+
+        Ok(transactions)
+    }
+
+    pub async fn scan_transactions(&mut self, transactions: Vec<monero::Transaction>) {
+        for tx in transactions {
+            let mut payment_id = PaymentId::zero();
+
+            // Get owned outputs.
+            let owned_outputs = tx.check_outputs(&self.viewpair, 0..1, 0..1).unwrap();
+
+            // Generate and display the SubFields (the parsed "extra" section) if applicable.
+            if owned_outputs.len() == 1 {
+                // Payments to integrated addresses only ever have one output.
+
+                // Get transaction's "extra" section.
+                let tx_extra = &tx.prefix().extra;
+
+                // Get vec of subfields from transaction's "extra" section.
+                let ExtraField(subfields) = tx_extra;
+
+                for subfield in subfields {
+                    if let SubField::Nonce(nonce_bytes) = subfield {
+                        // Shared secret can be retrieved as a combination of tx public key and your private view key.
+                        let shared_secret = tx.tx_pubkey().unwrap() * &(self.viewpair.view * 8u8);
+
+                        // The payment ID decryption key is a hash of the shared secret.
+                        let mut key_bytes = shared_secret.as_bytes().to_vec();
+                        key_bytes.append(&mut hex::decode("8d").unwrap());
+                        let key = keccak_256(&key_bytes);
+
+                        // The first byte of the nonce is not part of the encrypted payment ID.
+                        let mut id_bytes = nonce_bytes.clone()[1..9].to_vec();
+
+                        // Decrypt the payment ID by XORing it with the key.
+                        id_bytes
+                            .iter_mut()
+                            .zip(key.iter())
+                            .for_each(|(x1, x2)| *x1 ^= *x2);
+
+                        payment_id = PaymentId::from_slice(&id_bytes);
+                        println!("Payment ID: {}", hex::encode(&payment_id.as_bytes()))
+                    }
+                }
+            }
+
+            // If this payment is being tracked, update the amount paid.
+            if let Some(payment) = self.payments.get_mut(&payment_id) {
+                payment.paid_amount += owned_outputs[0]
+                    .amount()
+                    .expect("Failed to unblind transaction amount");
+            }
+        }
     }
 }
 
 #[derive(Default)]
 pub struct BlockScannerBuilder {
     daemon_url: String,
-    private_viewkey: Option<key::PrivateKey>,
-    public_spendkey: Option<key::PublicKey>,
+    private_viewkey: Option<monero::PrivateKey>,
+    public_spendkey: Option<monero::PublicKey>,
     scan_rate: Option<u64>,
 }
 
@@ -53,13 +196,13 @@ impl BlockScannerBuilder {
 
     pub fn private_viewkey(mut self, private_viewkey: &str) -> BlockScannerBuilder {
         self.private_viewkey =
-            Some(key::PrivateKey::from_str(&private_viewkey).expect("Invalid private viewkey"));
+            Some(monero::PrivateKey::from_str(&private_viewkey).expect("Invalid private viewkey"));
         self
     }
 
     pub fn public_spendkey(mut self, public_spendkey: &str) -> BlockScannerBuilder {
         self.public_spendkey =
-            Some(key::PublicKey::from_str(&public_spendkey).expect("Invalid public spendkey"));
+            Some(monero::PublicKey::from_str(&public_spendkey).expect("Invalid public spendkey"));
         self
     }
 
@@ -76,23 +219,37 @@ impl BlockScannerBuilder {
             .public_spendkey
             .expect("Private viewkey must be defined");
         let scan_rate = self.scan_rate.unwrap_or(1000);
-        let viewpair = key::ViewPair {
+        let viewpair = monero::ViewPair {
             view: private_viewkey,
             spend: public_spendkey,
         };
         BlockScanner {
             daemon_url: self.daemon_url,
             viewpair: viewpair,
+            payments: HashMap::new(),
             scan_rate: scan_rate,
         }
     }
 }
 
-struct Payment {
-    payment_id: address::PaymentId,
-    expected_amount: u64,
-    paid_amount: u64,
-    confirmations_required: u64,
-    confirmations_recieved: u64,
-    expiration_block: u64,
+pub struct Payment {
+    pub payment_id: PaymentId,
+    pub expected_amount: u64,
+    pub paid_amount: u64,
+    pub confirmations_required: u64,
+    pub confirmations_recieved: u64,
+    pub expiration_block: u64,
+}
+
+impl Payment {
+    pub fn new(amount: u64, confirmations: u64, expiration_block: u64) -> Payment {
+        Payment {
+            payment_id: PaymentId::random(),
+            expected_amount: amount,
+            paid_amount: 0,
+            confirmations_required: confirmations,
+            confirmations_recieved: 0,
+            expiration_block: expiration_block,
+        }
+    }
 }
