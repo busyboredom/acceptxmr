24e743f15478d87722f5f3713f06751b64182cb0
Charlie Wilkin <charlie@busyboredom.com>
2021-10-17 20:57:46 -0400

    Add Performance section and example.

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,29 @@
 //! calls in the scanning thread are logged on failure and repeated next scan. In the event that an
 //! error does occur, the liberal use of logging within this library will hopefully facilitate a
 //! speedy diagnosis an correction.
+//!
+//! ## Performance
+//!
+//! For maximum performance, host your own monero daemon on the same local network. Network and
+//! daemon slowness are primary cause of high payment update latency in the majority of use cases.
+//!
+//! To reduce the average latency before receiving payment updates, you may also consider lowering
+//! the [`PaymentGateway`]'s `scan_interval` below the default of 1 second:
+//! ```
+//! use acceptxmr::PaymentGateway;
+//! use std::time::Duration;
+//!
+//! let private_viewkey = "ad2093a5705b9f33e6f0f0c1bc1f5f639c756cdfc168c8f2ac6127ccbdab3a03";
+//! let public_spendkey = "7388a06bd5455b793a82b90ae801efb9cc0da7156df8af1d5800e4315cc627b4";
+//!
+//! let payment_gateway = PaymentGateway::builder(private_viewkey, public_spendkey)
+//!     .scan_interval(Duration::from_millis(100)) // Scan for payment updates every 100 ms.
+//!     .build();
+//! ```
+//!
+//! Please note that `scan_interval` is the minimum time before scanning for updates. If your
+//! daemon's response time is already greater than your `scan_interval`, or if your CPU is unable to
+//! scan new transactions fast enough, reducing your `scan_interval` will do nothing.
 
 #![warn(clippy::pedantic)]
 #![warn(missing_docs)]
