1152110d8b57ffb783c7fcbc1e54e11b4be8229f
Charlie Wilkin <charlie@busyboredom.com>
2023-08-07 23:31:53 -0400

    Add nix devshell

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ gcc rustup pkg-config openssl.dev ];
+        };
+      }
+    );
+}
